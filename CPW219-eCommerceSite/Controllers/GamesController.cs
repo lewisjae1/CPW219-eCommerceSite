878c45d52a99a3b777e0d125132525787153deeb
Jaein Lewis <dlwodls0115@gmail.com>
2022-02-21 23:30:01 -0800

    Added success message for Update Game

--- CPW219-eCommerceSite/Controllers/GamesController.cs
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -63,6 +63,7 @@ namespace CPW219_eCommerceSite.Controllers
                 _context.Games.Update(gameModel);
                 await _context.SaveChangesAsync();
 
+                TempData["Message"] = $"{gameModel.Title} was updated successfully!";
                 return RedirectToAction("Index");
             }
 
