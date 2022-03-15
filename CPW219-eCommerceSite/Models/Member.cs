8909fc0976247d9cdc8eda64ecdcc309596f5f63
Jaein Lewis <dlwodls0115@gmail.com>
2022-03-14 19:05:36 -0700

    Add Member class

---
+++ CPW219-eCommerceSite/Models/Member.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219_eCommerceSite.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberId { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string PassWord { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Username { get; set; }
+    }
+}
