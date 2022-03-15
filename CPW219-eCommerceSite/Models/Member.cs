8087f4fa53a527b8c745838427e3f52b221cb3bb
Jaein Lewis <dlwodls0115@gmail.com>
2022-03-14 19:52:15 -0700

    Added Register view

--- CPW219-eCommerceSite/Models/Member.cs
+++ CPW219-eCommerceSite/Models/Member.cs
@@ -21,20 +21,22 @@ namespace CPW219_eCommerceSite.Models
         [Required]
         [EmailAddress]
         [StringLength(100)]
-        [Display(Name = "Confirm Email")]
         public string Email { get; set; }
 
         [Required]
         [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
         public string ConfirmEmail { get; set; }
 
         [Required]
         [StringLength(75, MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
         [Compare(nameof (Password))]
         [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
     }
 }
